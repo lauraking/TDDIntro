585af23477941c1f3f2239a8fca7843ef0ecbf2d
lauraking <laurakingsley@Lauras-MacBook-Air-3.local>
2016-07-30 18:36:31 -0500

    FactorialTests: Pass 5 tests, added IllegalArgumentExceptionWhenNegative

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
         int factorial = 1;
         for (int j = 1; j <= i; j++) {
             factorial = factorial * j;
