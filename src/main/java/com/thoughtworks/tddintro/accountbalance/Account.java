7a437de02cc9131a6b87bb0726424dcbcdd7936a
lauraking <laurakingsley@Lauras-MacBook-Air-3.local>
2016-07-30 19:11:04 -0500

    AccountTests: balanceIncreaseFromDeposit

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by laurakingsley on 7/30/16.
+ */
+public class Account {
+    private int balance;
+    public Account(int i) {
+        balance = i;
+    }
+
+    public void deposit(int depositAmount) {
+         balance += depositAmount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+}
