cd16529e64315bed190299dde8d1e816584bae72
lauraking <laurakingsley@Lauras-MacBook-Air-3.local>
2016-08-04 16:53:29 -0500

    Recursive Factorial compute function

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -5,10 +5,23 @@ public class Factorial {
         if (i < 0) {
             throw new IllegalArgumentException();
         }
+        if (i == 0) return 1;
+        else {
+            return i * compute(i - 1);
+        }
+
+    }
+
+    public Integer computeIterative(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
         int factorial = 1;
         for (int j = 1; j <= i; j++) {
             factorial = factorial * j;
         }
         return factorial;
     }
+
+
 }
